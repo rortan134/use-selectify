b0e466058eceb55653a609d9cd7bb30530f6fa18
Rortan <samaritanojr006@gmail.com>
2023-03-22 23:26:05 +0100

    remove alternate box theme

--- src/stories/Selectify.stories.tsx
+++ src/stories/Selectify.stories.tsx
@@ -17,6 +17,18 @@ const Template = (args: UseSelectProps) => {
     } = useSelectify(containerRef, {
         ...args,
         exclusionZone: exclusionZoneRef.current,
+        onSelect: (el: Element) => {
+            el.classList.add("table-item-active");
+        },
+        onUnselect: (el: Element) => {
+            el.classList.remove("table-item-active");
+        },
+        onDragStart: () => {
+            document.body.style.userSelect = "none";
+        },
+        onDragEnd: () => {
+            document.body.style.userSelect = "auto";
+        },
     });
 
     function toggleItemSelection(elementToSelect: HTMLElement | null | undefined) {
@@ -34,63 +46,60 @@ const Template = (args: UseSelectProps) => {
     }
 
     return (
-        <div ref={containerRef} className="table-container">
-            <div className="table-content">
-                <header className="table-header">
-                    <div className="table-header-button">
-                        <button
-                            onClick={hasSelected ? clearSelection : selectAll}
-                            data-active={!hasSelected}
-                        >
-                            -
-                        </button>
-                    </div>
-                    <span>Type</span>
-                    <span>Group</span>
-                    <span>Serial.</span>
-                    <span>Code</span>
-                </header>
-
-                <div className="table-list" ref={exclusionZoneRef}>
-                    {[...Array(8)].map((_, i) => (
-                        <div key={i} className="table-item">
+        <React.StrictMode>
+            <div ref={containerRef} className="table-container">
+                <div className="table-content">
+                    <header className="table-header">
+                        <div className="table-header-button">
                             <button
-                                className="table-item-icon"
-                                onClick={(e) =>
-                                    toggleItemSelection(
-                                        e.currentTarget.parentElement // here we're getting the .table-item element to select
-                                    )
-                                }
+                                onClick={hasSelected ? clearSelection : selectAll}
+                                data-active={!hasSelected}
                             >
-                                <img src={SelectedCheckIcon} />
+                                -
                             </button>
-                            <span>Articulated</span>
-                            <span>Virtual Factory</span>
-                            <span>KR-{i + 1 * 1234}</span>
-                            <span>{(i + 200 * 4321).toFixed(0)}</span>
                         </div>
-                    ))}
+                        <span>Type</span>
+                        <span>Group</span>
+                        <span>Serial.</span>
+                        <span>Code</span>
+                    </header>
+
+                    <div className="table-list" ref={exclusionZoneRef}>
+                        {[...Array(32)].map((_, i) => (
+                            <div key={i} className="table-item">
+                                <button
+                                    className="table-item-icon"
+                                    onClick={(e) =>
+                                        toggleItemSelection(
+                                            e.currentTarget.parentElement // here we're getting the .table-item element to select
+                                        )
+                                    }
+                                >
+                                    <img src={SelectedCheckIcon} />
+                                </button>
+                                <span>Articulated</span>
+                                <span>Virtual Factory</span>
+                                <span>KR-{i + 1 * 1234}</span>
+                                <span>{(i + 200 * 4321).toFixed(0)}</span>
+                            </div>
+                        ))}
+                    </div>
                 </div>
+                <SelectBoxOutlet />
             </div>
-            <SelectBoxOutlet />
-        </div>
+        </React.StrictMode>
     );
 };
 
 export const Basic = Template.bind({}) as ComponentStory<any>;
 Basic.args = {
     selectCriteria: `.table-item`,
-    onSelect: (el: Element) => {
-        el.classList.add("table-item-active");
-    },
-    onUnselect: (el: Element) => {
-        el.classList.remove("table-item-active");
-    },
-    onDragStart: () => {
-        document.body.style.userSelect = "none";
-    },
-    onDragEnd: () => {
-        document.body.style.userSelect = "auto";
+};
+Basic.parameters = {
+    docs: {
+        source: {
+            code: "Disabled for this story, see https://github.com/storybookjs/storybook/issues/11554",
+        },
     },
 };
 
@@ -98,12 +107,12 @@ export const SecondTheme = Template.bind({}) as ComponentStory<any>;
 SecondTheme.args = {
     selectCriteria: `.table-item`,
     theme: "outline",
-    autoScroll: false,
-    onSelect: (el: Element) => {
-        el.classList.add("table-item-active");
-    },
-    onUnselect: (el: Element) => {
-        el.classList.remove("table-item-active");
+};
+SecondTheme.parameters = {
+    docs: {
+        source: {
+            code: "Disabled for this story, see https://github.com/storybookjs/storybook/issues/11554",
+        },
     },
 };
 
@@ -111,12 +120,12 @@ export const FullInsersectionOnly = Template.bind({}) as ComponentStory<any>;
 FullInsersectionOnly.args = {
     selectCriteria: `.table-item`,
     onlySelectOnFullOverlap: true,
-    autoScroll: false,
-    onSelect: (el: Element) => {
-        el.classList.add("table-item-active");
-    },
-    onUnselect: (el: Element) => {
-        el.classList.remove("table-item-active");
+};
+FullInsersectionOnly.parameters = {
+    docs: {
+        source: {
+            code: "Disabled for this story, see https://github.com/storybookjs/storybook/issues/11554",
+        },
     },
 };
 
@@ -124,12 +133,12 @@ export const OnMetaKeyOnly = Template.bind({}) as ComponentStory<any>;
 OnMetaKeyOnly.args = {
     selectCriteria: `.table-item`,
     activateOnMetaKey: true,
-    autoScroll: false,
-    onSelect: (el: Element) => {
-        el.classList.add("table-item-active");
-    },
-    onUnselect: (el: Element) => {
-        el.classList.remove("table-item-active");
+};
+OnMetaKeyOnly.parameters = {
+    docs: {
+        source: {
+            code: "Disabled for this story, see https://github.com/storybookjs/storybook/issues/11554",
+        },
     },
 };
 
@@ -137,11 +146,12 @@ export const OnlySelectOnDragEnd = Template.bind({}) as ComponentStory<any>;
 OnlySelectOnDragEnd.args = {
     selectCriteria: `.table-item`,
     onlySelectOnDragEnd: true,
-    onSelect: (el: Element) => {
-        el.classList.add("table-item-active");
-    },
-    onUnselect: (el: Element) => {
-        el.classList.remove("table-item-active");
+};
+OnlySelectOnDragEnd.parameters = {
+    docs: {
+        source: {
+            code: "Disabled for this story, see https://github.com/storybookjs/storybook/issues/11554",
+        },
     },
 };
 
@@ -154,6 +164,13 @@ WithDelay.args = {
     },
     onUnselect: (el: Element) => {
         el.classList.remove("table-item-active");
+    },
+};
+WithDelay.parameters = {
+    docs: {
+        source: {
+            code: "Disabled for this story, see https://github.com/storybookjs/storybook/issues/11554",
+        },
     },
 };
 
